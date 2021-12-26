3134565d27d7be68024a103d8da83fbcf753e958
Lincoln Ruteski <lruteski@gmail.com>
2021-12-26 06:51:52 -0300

    aula sobre comentários

---
+++ sessao2/1_12-Comentarios/aula12.py
@@ -0,0 +1,18 @@
+# isso é um comentário de uma linha
+print('linha 1')  # isso é um comentário, 2 espaços por causa do PEP8
+#  print('linha 2')
+
+""" comentario de multiplas linhas
+print('linha 3')
+print('linha 4')
+"""
+
+'''
+print('linha 5')
+print('linha 6')
+'''
+
+#  quebra de linha
+print('linha \n7')
+
+print('linha 8')
