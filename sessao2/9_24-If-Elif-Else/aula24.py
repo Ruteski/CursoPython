72b32545194a18d68d61929c35fc9efae46ca9fc
Lincoln Ruteski <lruteski@gmail.com>
2021-12-26 12:28:03 -0300

    if else elif com varias condições

---
+++ sessao2/9_24-If-Elif-Else/aula24.py
@@ -0,0 +1,37 @@
+"""
+condições if, elif e else
+"""
+
+# BOOELANO
+"""
+if True:
+    print('Verdadeiro')
+
+    num_1 = 2
+    num_2 = 4
+
+    print(num_1 + num_2)
+else:
+    print('a minha expressao nao é verdadeira')
+
+
+if False:
+    print('verdadeiro')
+elif True:
+    print('agora é verdadeiro')
+elif True:
+    print('mais uma verdadeira')
+else:
+    print('nao é verdadeiro')
+"""
+
+# OPERADORES RELACIONAIS
+"""
+== > >= < <= !=
+"""
+
+# OPERADORES LOGICOS
+"""
+and, or, not
+in e not in
+"""
