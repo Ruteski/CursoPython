8de7317a40a3c56fadde9c90cfd6409bf299d135
Lincoln Ruteski <lruteski@gmail.com>
2021-12-26 08:53:25 -0300

    entrada de dados

---
+++ sessao2/8_23-Input/aula23.py
@@ -0,0 +1,28 @@
+"""
+Entrada de dados - input
+"""
+
+"""
+nome = input('Qual o seu nome? \nR: ')
+idade = int(input('Qual sua idade: \nR: '))
+ano_nasc = int(input('Qual o ano de nascimento: \nR: '))
+
+
+print()
+# print('Nome digitado:', nome)
+print(f'Nome digitado: {nome}')
+
+# print('Idade digitada:', idade)
+print(f'Idade digitada {idade}')
+
+print()
+print(f'{nome} tem {idade} anos de idade e nasceu em {ano_nasc}')
+"""
+
+# calculadora
+numero_1 = input('Digite um numero: ')
+numero_2 = float(input('Digite outro numero: '))
+total = float(numero_1) + numero_2
+
+print(f'A soma entre {numero_1} e {numero_2} é: {total}')
+
