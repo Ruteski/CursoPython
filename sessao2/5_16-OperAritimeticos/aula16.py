ce7d463267243f321ce02d3d66d8b0238fecb06b
Lincoln Ruteski <lruteski@gmail.com>
2021-12-26 08:02:32 -0300

    operadores aritméticos

---
+++ sessao2/5_16-OperAritimeticos/aula16.py
@@ -0,0 +1,26 @@
+"""
++
+-
+*
+/
+// divisao inteira(arredonda o resultado para numero inteiro)
+** exponenciacao/potenciacao
+% resto da divisao
+()
+"""
+print('10 + 10 = ', 10 + 10)
+print('10 - 10 = ', 10 - 10)
+print('10 * 10 = ', 10 * 10)
+print('10 / 5 = ', 10 / 5)
+print('10 / 3 = ', 10 / 3)
+print('10 // 3 = ', 10 // 3)
+print('10 // 2 = ', 10 // 2)
+print('10 % 2 = ', 10 % 2)
+print('10 ** 2 = ', 10 ** 2)
+print('Multiplicação * ', 3 * '13')
+print('5' + '5')
+print(5+2*10)
+print((5+2)*10)
+
+
+
