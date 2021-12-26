05e7d1443ce3ce765f15b331b91df8eb6666ae9d
Lincoln Ruteski <lruteski@gmail.com>
2021-12-26 07:21:02 -0300

    strings e aspas

---
+++ sessao2/3_14-Strings-e-aspas/aula14.py
@@ -0,0 +1,14 @@
+"""
+str - string
+"""
+
+print('aspas simples')
+print("aspas duplas")
+print(123456)  # integer, conversao implicita para string
+print("essa é uma 'string' (str).")
+print('essa é uma "string" (str).')
+print('essa é uma \'string\' (str).')
+print("essa é uma \"string\" (str).")  # '\'  caracter de escape
+print('essa é uma \n (str).')
+print(r'essa é uma \n (str).')  # o 'r' no inicio ignora oq tem dentro e gera tudo como string
+
