2849cc09e7e892ca34f67687664f769fb3bb288b
Lincoln Ruteski <lruteski@gmail.com>
2021-12-26 07:07:35 -0300

    comando print

---
+++ sessao2/1_13-Print/aula13.py
@@ -0,0 +1,13 @@
+# print(123456)
+# print('Lincoln', 'Ruteski')
+# print('Lincoln', 'Ruteski', sep='-')
+"""
+print('Jao', 'e', 'Maria', sep='-', end='   ')  # o 'end' envia qualquer coisa no final do print, querbra de linha é padrao, posso enviar em branco para não quebrar a linha
+print('Zé', 'Pequeno', sep='-', end=' ### ')
+"""
+
+"""
+824.176.070-18
+"""
+print('824', '176', '070', sep='.', end='-')
+print('18')
