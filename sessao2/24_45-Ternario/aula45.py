dc7745e1e0ad47c86c85432209cec588a3fd42c6
Lincoln Ruteski <lruteski@gmail.com>
2021-12-29 18:11:13 -0300

    operador ternario

---
+++ sessao2/24_45-Ternario/aula45.py
@@ -0,0 +1,10 @@
+logado = False
+
+# if logado:
+#     msg = 'usuario logado'
+# else:
+#     msg = 'usuario precisa logar'
+
+msg = 'usuario logado' if logado else 'usuario precisa fazer login'
+
+print(msg)
