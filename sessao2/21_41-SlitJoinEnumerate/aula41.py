4ae656f0e1ddd8feb89e4cf8b54dd589a09bcd41
Lincoln Ruteski <lruteski@gmail.com>
2021-12-29 16:51:06 -0300

    split join e enumerate string e lista

---
+++ sessao2/21_41-SlitJoinEnumerate/aula41.py
@@ -0,0 +1,63 @@
+# string = 'o brasil é o pais do futebal, o brasil é penta.'
+# lista1 = string.split(' ')
+# lista2 = string.split(',')
+
+# print(lista2)
+
+# palavra = ''
+# contagem = 0
+#
+# for valor in lista1:
+#     # print(valor)
+#     # print(f'a palavra "{valor}" apareceu {lista1.count(valor)}x na frase.')
+#     qtd_vezes = lista1.count(valor)
+#
+#     if qtd_vezes > contagem:
+#         contagem = qtd_vezes
+#         palavra = valor
+#
+# print(f'a palavra que apareceu mais vezes é "{palavra}" ({contagem}x)')
+#
+
+# #### JOIN
+# string = 'o brasil é penta.'
+# lista = string.split(' ')
+# string2 = ','.join(lista)
+#
+# print(string2)
+
+# ### ENUMERATE
+# string = 'o brasil é penta.'
+# lista = string.split(' ')
+#
+# for indice, valor in enumerate(lista):
+#     print(indice, valor, lista[indice])
+
+# lista = [
+#     [1,2],
+#     [3,4],
+#     [5,6],
+# ]
+#
+# for v in lista:
+#     # print(v)
+#     print(v[0], v[1])
+
+
+# lista2 = [
+#     [0,'lincoln'],
+#     [1, 'joao'],
+#     [2, 'maria'],
+# ]
+#
+# for indice, nome in lista2:
+#     print(indice, nome)
+
+lista = ['lincoln', 'joao', 'maria']
+
+# for indice, nome in enumerate(lista):
+#     print(indice, nome)
+
+n1, n2, n3 = lista
+
+print(n2)
