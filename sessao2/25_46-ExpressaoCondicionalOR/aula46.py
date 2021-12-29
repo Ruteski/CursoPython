6f90ca819c2c305b120d9bcfbd8caae6134e0370
Lincoln Ruteski <lruteski@gmail.com>
2021-12-29 18:18:58 -0300

    expressao condicional com OR

---
+++ sessao2/25_46-ExpressaoCondicionalOR/aula46.py
@@ -0,0 +1,14 @@
+# nome = input('qual seu nome? ')
+# print(nome or None or False or 0 or 'voce nao digitou nada' or 'outra coisa')
+
+a = 0
+b = None
+c = False
+d = []
+e = {}
+f = 22
+g = 'lincoln'
+
+variavel = a or b or c or d or e or f or g
+
+print(variavel)
