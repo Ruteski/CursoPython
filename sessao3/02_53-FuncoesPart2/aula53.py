8b9e8a5c2573a911f6d30b73191af3849a8c7af3
Lincoln Ruteski <lruteski@gmail.com>
2021-12-30 12:01:15 -0300

    correcao de pastas e commits

---
+++ sessao3/02_53-FuncoesPart2/aula53.py
@@ -0,0 +1,26 @@
+# def funcao(var):
+#     # pass
+#     return var
+#
+#
+# variavel = funcao("Valor que eu quero")
+#
+# if variavel:
+#     print(variavel)
+# else:
+#     print('nenhum valor')
+
+def divisao(n1, n2):
+    if n2 == 0:
+        return
+
+    return n1/n2
+
+
+divide = divisao(8,0)
+
+if divide:
+    print(divide)
+else:
+    print('conta invalida')
+
