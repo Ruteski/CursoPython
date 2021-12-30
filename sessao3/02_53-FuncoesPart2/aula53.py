7fe87d6559429c5391f605c451b11561f6235ec9
Lincoln Ruteski <lruteski@gmail.com>
2021-12-30 13:19:11 -0300

    funcoes parte 2

--- sessao3/02_53-FuncoesPart2/aula53.py
+++ sessao3/02_53-FuncoesPart2/aula53.py
@@ -10,17 +10,34 @@
 # else:
 #     print('nenhum valor')
 
-def divisao(n1, n2):
-    if n2 == 0:
-        return
+# def divisao(n1, n2):
+#     if n2 == 0:
+#         return
 
-    return n1/n2
+#     return n1/n2
+
+# divide = divisao(8,0)
+
+# if divide:
+#     print(divide)
+# else:
+#     print('conta invalida')
+
+def f(var):
+    print(var)
 
 
-divide = divisao(8,0)
+def dumb():
+    return f
 
-if divide:
-    print(divide)
-else:
-    print('conta invalida')
 
+# var = dumb()('e colocar meu valor aqui')
+var = dumb()
+# print(id(var), id(f))
+
+# if var == f:
+#     print('var é igual a f')
+# else:
+#     print('blaaaaaa')
+
+var('pode imprimir algo na tela')
