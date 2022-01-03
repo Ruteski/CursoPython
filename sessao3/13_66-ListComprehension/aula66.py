4aef559fded3e384256e8e11e975e73eb5a7095e
Lincoln Ruteski <lruteski@gmail.com>
2022-01-03 17:21:28 -0300

    mais sobre o exercicio

--- sessao3/13_66-ListComprehension/aula66.py
+++ sessao3/13_66-ListComprehension/aula66.py
@@ -26,7 +26,7 @@ ex7 = [v if v % 3 == 0 else f'{v} - não é' for v in l3]
 
 ex8 = [v if v % 3 == 0 and v % 8 == 0 else f'{v} - não é' for v in l3]
 
-print(ex8)
+# print(ex8)
 
 """
 exercicio
@@ -35,8 +35,12 @@ string = '012345678901234567890123456789012345678901234567890123456789'
 
 string = '012345678901234567890123456789012345678901234567890123456789'
 n = 10
+contadores = [i for i in range(0, len(string), n)]
+tuplas = [(i, i + n) for i in range(0, len(string), n)]
 lista = [string[i:i+n] for i in range(0, len(string), n)]
 retorno = '.'.join(lista)
 
+print(contadores)
+print(tuplas)
 print(lista)
 print(retorno)
