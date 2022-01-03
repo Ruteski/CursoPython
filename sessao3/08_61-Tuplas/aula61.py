2535fe5a237706cff4bc356e97735afb551e3f3e
Lincoln Ruteski <lruteski@gmail.com>
2022-01-03 13:26:33 -0300

    tupla

---
+++ sessao3/08_61-Tuplas/aula61.py
@@ -0,0 +1,40 @@
+# t1 = (1,2,3, 'a', 'ruteski', 56.69, False)
+
+# print(type(t1))
+# print(t1)
+# print(t1[4])
+
+# for v in t1:
+#     print(v)
+#
+
+# print(t1[2:6])
+
+# t2 = 2,1,'a','b'
+#
+# print(t2)
+
+# t1 = 1,
+# print(t1, type(t1))
+
+# t1 = 1,2,'lincoln',4,5
+# t2 = 6,'ruteski',8,9,10
+# t3 = t1 + t2
+#
+# # print(t3)
+#
+# n1, n2, n3, *n, n9, n10 = t3
+# print(n1, n2, n3, n9, n10, n)
+#
+# t1 = (1,2,'ruteski') * 20
+#
+# print(t1)
+
+t1 = (1,2,3,4,5,6)
+print(t1)
+t1 = list(t1)
+t1[1] = 45
+print(t1)
+t1 = tuple(t1)
+print(t1)
+
