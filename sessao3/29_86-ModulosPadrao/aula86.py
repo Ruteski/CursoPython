39ec623a9909edfc1cc3de925fe70101ba8ec39a
Lincoln Ruteski <lruteski@gmail.com>
2022-01-04 16:24:20 -0300

    modulos padrao python

---
+++ sessao3/29_86-ModulosPadrao/aula86.py
@@ -0,0 +1,16 @@
+"""
+modulos padrao sao arquivos .py e servem para expandir as funcionalidades padrao da linguagem
+
+veja todos os modulos padrao do python em:
+    https://docs.python.org/3/py-modindex.html
+"""
+
+# import sys
+# print(sys.platform)
+
+# from sys import platform
+# print(platform)
+
+from random import randint, random
+for i in range(10):
+    print(randint(0,10), random())
