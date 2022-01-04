0bf112f4127d949cf8f822b91b2fc28d766b9b45
Lincoln Ruteski <lruteski@gmail.com>
2022-01-04 17:09:59 -0300

    criar modulo

---
+++ sessao3/30_87-CriandoModulos/aplicativo.py
@@ -0,0 +1,12 @@
+# import aula87 as calculos
+#
+# print(calculos.PI)
+#
+# lista = [2,4]
+# print(calculos.multiplica(lista))
+
+
+
+from aula87 import dobra_lista, PI, multiplica
+
+print(multiplica([2,4]))
