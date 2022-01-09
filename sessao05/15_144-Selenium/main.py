a8dc70a6b92dcc14cb0bc963a0188de3b6ff9358
Lincoln Ruteski <lruteski@gmail.com>
2022-01-09 11:08:03 -0300

    acertos para rodar em windows

--- sessao05/15_144-Selenium/main.py
+++ sessao05/15_144-Selenium/main.py
@@ -4,7 +4,8 @@ from time import sleep
 
 class ChromeAuto:
     def __init__(self):
-        self.driver_path = 'chromedriver'
+        # no linux é apenas o chromedriver , ir no site pypi.org, procurar pelo selenium e baixar o driver
+        self.driver_path = 'chromedriver.exe'
         self.options = webdriver.ChromeOptions()
         self.options.add_argument('user-data-dir=Perfil')
         self.chrome = webdriver.Chrome(
